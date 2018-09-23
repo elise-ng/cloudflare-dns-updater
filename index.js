47f2ce48659b235b83d1d3521e9819d3f32da684
Elliot Ng <chihimng.io@gmail.com>
2018-09-23 18:45:49 +0800

    feat: rewrite everything

--- index.js
+++ index.js
@@ -1,114 +1,48 @@
-let request = require('request')
-let config = require('./config.json')
-if (!config.hostname || !config.email || !config.token) {
-  console.log('Config missing. Please fill in config file.')
-  process.exit()
-}
-const CFAuthHeader = {
-  'X-Auth-Email': config.email,
-  'X-Auth-Key': config.token
-}
-var myIP = ''
-var zoneID = ''
-var recordID = ''
-// Get Public IP
-let getPublicIP = new Promise((resolve, reject) => {
-  let req = {
-    url: 'https://api.ipify.org?format=json',
-    json: true
-  }
-  request.get(req, (error, response, body) => {
-    if (error) {
-      console.log('Error getting public ip address: ', error)
-      reject()
-    }
-    if (response.statusCode < 200 && response.statusCode > 299) {
-      console.log(`Error getting public ip address: HTTP Error ${response.statusCode}`)
-      reject()
-    }
-    myIP = body['ip']
-    console.log(`Public IP Address: ${myIP}`)
-    resolve()
-  })
-})
+const axios = require('axios')
+const publicIp = require('public-ip')
+const config = require('./config.json')
 
-let getIDs = new Promise((resolve, reject) => {
-  // Get Zone ID
-  let req = {
-    method: 'GET',
-    url: `https://api.cloudflare.com/client/v4/zones?name=${encodeURI(`${config.hostname.split('.').reverse()[1]}.${config.hostname.split('.').reverse()[0]}`)}`,
-    headers: CFAuthHeader,
-    json: true
-  }
-  request(req, (error, response, body) => {
-    if (error) {
-      console.log('Error getting Zone ID: ', error)
-      reject()
+async function main () {
+  try {
+    // Load Config
+    if (!config.hostname || !config.email || !config.token) { throw Error('Config missing') }
+    const cfAuthHeaders = {
+      'X-Auth-Email': config.email,
+      'X-Auth-Key': config.token
     }
-    if (response.statusCode < 200 && response.statusCode > 299) {
-      console.log(`Error getting Zone ID: HTTP Error ${response.statusCode}`)
-      reject()
-    }
-    if (body['result'].length === 0) {
-      console.log('Error: Zone not found')
-      reject()
-    }
-    zoneID = body['result'][0]['id']
-    console.log(`Zone ID: ${zoneID}`)
+
+    // Get Public IP and Zone ID
+    const cfZoneIdReqUrl = `https://api.cloudflare.com/client/v4/zones?name=${encodeURI(`${config.hostname.split('.').reverse()[1]}.${config.hostname.split('.').reverse()[0]}`)}`
+    const [ip, cfZoneIdRes] = await Promise.all([publicIp.v4(), axios.get(cfZoneIdReqUrl, { headers: cfAuthHeaders })])
+    console.log('Public IP: ', ip)
+    if (cfZoneIdRes.data.result.length <= 0) { throw Error('Zone not found') }
+    const cfZoneId = cfZoneIdRes.data.result[0].id
+    console.log('Zone ID: ', cfZoneId)
+
     // Get DNS Record ID
-    let req = {
-      method: 'GET',
-      url: `https://api.cloudflare.com/client/v4/zones/${encodeURI(zoneID)}/dns_records?name=${encodeURI(config.hostname)}`,
-      headers: CFAuthHeader,
-      json: true
-    }
-    request(req, (error, response, body) => {
-      if (error) {
-        console.log('Error getting DNS record id: ', error)
-        reject()
-      }
-      if (response.statusCode < 200 && response.statusCode > 299) {
-        console.log(`Error getting DNS record id: HTTP Error ${response.statusCode}`)
-        reject()
-      }
-      if (body['result'].length === 0) {
-        console.log('Error: DNS record not found, have you set up your hostname on CloudFlare yet?')
-        reject()
-      }
-      recordID = body['result'][0]['id']
-      console.log(`DNS Record ID: ${recordID}`)
-      resolve()
-    })
-  })
-})
+    const cfDnsIdReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records?name=${encodeURI(config.hostname)}`
+    const cfDnsIdRes = await axios.get(cfDnsIdReqUrl, { headers: cfAuthHeaders })
+    if (cfDnsIdRes.data.result.length <= 0) { throw Error('DNS record not found') }
+    const cfDnsId = cfDnsIdRes.data.result[0].id
+    console.log('DNS Record ID: ', cfDnsId)
 
-Promise.all([getPublicIP, getIDs]).then(result => {
-  // Update DNS Record
-  let req = {
-    method: 'PUT',
-    url: `https://api.cloudflare.com/client/v4/zones/${encodeURI(zoneID)}/dns_records/${encodeURI(recordID)}`,
-    headers: CFAuthHeader,
-    json: {
+    // Update DNS Record
+    const cfPutReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records/${encodeURI(cfDnsId)}`
+    const cfPutReqData = {
       'type': 'A',
       'name': config.hostname,
-      'content': myIP
+      'content': ip
     }
-  }
-  request(req, (error, response, body) => {
-    if (error) {
-      console.log('Error updating DNS record: ', error)
-      process.exit()
-    }
-    if (response.statusCode < 200 && response.statusCode > 299) {
-      console.log(`Error updating DNS record: HTTP Error ${response.statusCode}`)
-      process.exit()
-    }
-    if (body['success']) {
-      console.log('DNS Record updated successfully.')
+    const cfPutRes = await axios.put(cfPutReqUrl, cfPutReqData, { headers: cfAuthHeaders })
+    if (cfPutRes.data.success === true) {
+      console.log('DNS Record update success: ', JSON.stringify(cfPutRes.data, undefined, 2))
     } else {
-      console.log('Error updating DNS record: ', body['errors'][0]['message'])
+      console.error('DNS Record update failed: ', JSON.stringify(cfPutRes.data.errors, undefined, 2))
     }
-  })
-}).catch(error => {
-  console.log('Fatal error occured. Exiting...')
-})
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// entry
+main()
