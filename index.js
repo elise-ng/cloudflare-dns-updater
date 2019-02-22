fae8761620daf70e04b95dbb70a9b3d3b79d77b6
Elliot Ng <chihimng.io@gmail.com>
2019-02-22 23:52:41 +0800

    feat: support AAAA records

--- index.js
+++ index.js
@@ -23,22 +23,40 @@ async function main () {
     const cfDnsIdReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records?name=${encodeURI(config.hostname)}`
     const cfDnsIdRes = await axios.get(cfDnsIdReqUrl, { headers: cfAuthHeaders })
     if (cfDnsIdRes.data.result.length <= 0) { throw Error('DNS record not found') }
-    const cfDnsId = cfDnsIdRes.data.result[0].id
-    console.log('DNS Record ID: ', cfDnsId)
-
-    // Update DNS Record
-    const cfPutReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records/${encodeURI(cfDnsId)}`
-    const cfPutReqData = {
-      'type': 'A',
-      'name': config.hostname,
-      'content': ip
-    }
-    const cfPutRes = await axios.put(cfPutReqUrl, cfPutReqData, { headers: cfAuthHeaders })
-    if (cfPutRes.data.success === true) {
-      console.log('DNS Record update success: ', JSON.stringify(cfPutRes.data, undefined, 2))
-    } else {
-      console.error('DNS Record update failed: ', JSON.stringify(cfPutRes.data.errors, undefined, 2))
-    }
+    const results = await Promise.all(cfDnsIdRes.data.result.map(async cfDnsRecord => {
+      console.log('DNS Record ID: ', cfDnsRecord.id)
+      let content
+      switch (cfDnsRecord.type) {
+        case 'A':
+          content = await publicIp.v4()
+          break
+        case 'AAAA':
+          content = await publicIp.v6()
+          break
+        default:
+          console.error(`DNS Record Type unsupported: ${cfDnsRecord.type}`)
+          return
+      }
+      // Update DNS Record
+      const cfPutReqUrl = `https://api.cloudflare.com/client/v4/zones/${encodeURI(cfZoneId)}/dns_records/${encodeURI(cfDnsRecord.id)}`
+      const cfPutReqData = {
+        'type': cfDnsRecord.type,
+        'name': cfDnsRecord.name,
+        'content': content
+      }
+      return axios.put(cfPutReqUrl, cfPutReqData, { headers: cfAuthHeaders })
+    }))
+    results.forEach(result => {
+      if (!result || !result.data) {
+        console.error(`Warning: null result received, see above for error messages`)
+        return
+      }
+      if (result.data.success === true) {
+        console.log(`DNS Record update success: `, JSON.stringify(result.data, undefined, 2))
+      } else {
+        console.error(`DNS Record update failed: `, JSON.stringify(result.data.errors, undefined, 2))
+      }
+    })
   } catch (e) {
     console.error(e)
   }
